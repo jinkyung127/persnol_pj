ec05900f7527e50ad42015a4922dc2d3f58be7b8
Kim Jinkyung <jinkyung127@naver.com>
2023-05-30 21:08:24 +0900

    captions add

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const options = {
   },
 };
 
+// 영화 api 받아서 카드 붙이기
 function listing() {
   fetch(
     "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1",
@@ -16,8 +17,8 @@ function listing() {
     .then((data) => {
       console.log(data)
       let rows = data["results"];
-      const cardBox = document.querySelector("#cards-box");
-      cardBox.innerHTML = '';
+      const cardBox = document.querySelector("#cards-box"); 
+      cardBox.innerHTML = ''; // 기존 카드 지우기
       rows.forEach((a) => {
         let title = a["title"];
         let content = a["overview"];
@@ -36,23 +37,23 @@ function listing() {
                                     </div>
                                 </div>
                             </div>`;
-        cardBox.insertAdjacentHTML('beforeend', temp_html);
-        const clickCardBox = cardBox.lastElementChild;
-        clickCardBox.addEventListener('click', () => clickCard(id));
+        cardBox.insertAdjacentHTML('beforeend', temp_html); // jquery의 append기능
+        const clickCardBox = cardBox.lastElementChild; // 카드 클릭 함수 실행할 변수 선언
+        clickCardBox.addEventListener('click', () => clickCard(id)); // 카드 클릭 이벤트 실행
       });
     });
 }
-
-function clickCard(id) {
+// alert 카드 id 함수 선언
+function clickCard(id) { 
   alert(`id = ${id}`);
 }
-
+// 카드 붙이는 함수 실행
 listing();
-
+// 검색 함수 선언
 function searchMovie() {
-  const searchBox = document.getElementById('searchinput').value;
-  const movieCardBox = document.getElementById('cards-box');
-  movieCardBox.innerHTML = '';
+  const searchBox = document.getElementById('searchinput').value; // 검색 input값 받아오기
+  const movieCardBox = document.getElementById('cards-box'); // html에서 카드 찾기
+  movieCardBox.innerHTML = ''; // 기존 카드 지우기
 
   fetch(
     "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1",
@@ -62,9 +63,10 @@ function searchMovie() {
     .then((data) => {
       let results = data['results'];
       if (searchBox.length === 0) {
-        alert('한 글자 이상 적어주세요');
+        alert('한 글자 이상 적어주세요'); // 입력값 없는 경우
       }
       const filteredResults = results
+      // map() 으로 카드 배열 생성
         .map((item) => ({
           title: item['title'],
           content: item['overview'],
@@ -72,11 +74,13 @@ function searchMovie() {
           rate: item['vote_average'],
           id: item['id'],
         }))
-        .filter((movie) => movie.title.includes(searchBox));
+        // filter() 로 검색 인풋이 포함된 타이틀이 있는 배열만 반환
+        .filter((movie) => movie.title.toLowerCase().includes(searchBox.toLowerCase()));
       if (filteredResults.length === 0) {
-        alert('일지하는 검색결과가 없습니다');
+        alert('일지하는 검색결과가 없습니다'); // 인풋이 포함된 제목 값이 없을 경우
         window.location.reload();
       }
+      // 필터링한 배열만 카드로 붙이기
       filteredResults.forEach((movie) => {
         let temp_html = `<div class="col">
                                 <div class="card h-100">
@@ -95,10 +99,10 @@ function searchMovie() {
       });
     });
 }
-
+// 검색 버튼 이벤트 함수 실행
 const searchButton = document.getElementById('searchbtn');
 searchButton.addEventListener('click', searchMovie);
-
+// 엔터 키 입력 = 검색 버튼 클릭
 const searchIpt = document.getElementById('searchinput');
 searchIpt.addEventListener('keypress', (event) => {
   if (event.key === 'Enter') {
@@ -106,7 +110,7 @@ searchIpt.addEventListener('keypress', (event) => {
     searchMovie();
   }
 });
-
+// 제목 클릭 -> 새.고
 const main = () => {
   window.location.reload();
 };
